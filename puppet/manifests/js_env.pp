17ee2c934d154981ce0f3db9c487b4de9d374d68
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2014-10-15 08:14:37 +0300

    removes duplicate git package dependency

--- puppet/manifests/js_env.pp
+++ puppet/manifests/js_env.pp
@@ -1,4 +1,4 @@
-$package_list = ["ubuntu-desktop", "virtualbox-guest-x11", "gnome-terminal", "git", "kdiff3"]
+$package_list = ["ubuntu-desktop", "virtualbox-guest-x11", "gnome-terminal", "kdiff3"]
 
 package { $package_list:
 	ensure => latest
